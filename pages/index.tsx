31bbdce49ab0ae3db42fa31854a579ab75182807
tulakann <tulakan.r@gmail.com>
2022-05-07 11:34:38 +0700

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-8">
+      <h1 className="mb-2 text-2xl font-bold">Next.js + Tailwind CSS</h1>
+      <p>Enjoy starting a new project!</p>
+    </div>
+  );
+};
+
+export default Home;
