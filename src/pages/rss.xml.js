137a45e5eed963d05aa79b74a353778492370257
bluenex <tulakan.r@gmail.com>
2026-04-28 14:00:40 +0700

    refactor: switch RSS feed to getCollection()

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,20 +1,21 @@
 import rss from "@astrojs/rss";
-import dayjs from "dayjs";
-import customParseFormat from "dayjs/plugin/customParseFormat.js";
+import { getCollection } from "astro:content";
 import { SITE_DESCRIPTION, SITE_TITLE } from "@/consts";
-import { getPostList } from "@/libs/posts";
-
-dayjs.extend(customParseFormat);
+import { entryToPostListItem } from "@/libs/posts";
 
 export async function GET(context) {
-  const posts = getPostList();
+  const entries = await getCollection("posts");
+  const posts = entries
+    .map(entryToPostListItem)
+    .sort((a, b) => b.date.getTime() - a.date.getTime());
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
     items: posts.map((post) => ({
       title: post.title,
-      pubDate: dayjs(post.date, "DD-MM-YYYY HH:mm").toDate(),
+      pubDate: post.date,
       description: post.excerpt,
       link: `/blog/${post.id}/`,
     })),
